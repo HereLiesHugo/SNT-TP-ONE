06565a0d27f8dbb412b2fc0597c45428a39b724f
Hugo <84995264+HereLiesHugo@users.noreply.github.com>
2023-10-20 11:29:53 +0200

    Add files via upload

---
+++ exo8.py
@@ -0,0 +1,28 @@
+#pour 4:
+eNum=4
+eNum=eNum*5
+eNum=eNum+7
+eNum=eNum*4
+eNum=eNum+6
+eNum=eNum*5
+print("Pour 4: ", eNum)
+
+eNum=int(input("Entrez un nombre entier: "))
+eNum=eNum*5
+eNum=eNum+7
+eNum=eNum*4
+eNum=eNum+6
+eNum=eNum*5
+print("Réponse: ", eNum)
+
+#eNum c'est la meme chose que utiliser, x, 
+#car x est une varable, tout ce qui change est le nom de variable
+
+#pour x
+x=int(input("Entrez un nombre entier: "))
+x=x*5
+x=x+7
+x=x*4
+x=x+6
+x=x*5
+print(x)
