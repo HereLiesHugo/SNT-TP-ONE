55b93bd63983e71fcd8e22e5dc06bacb2fc0680d
Hugo <84995264+HereLiesHugo@users.noreply.github.com>
2023-10-20 11:10:09 +0200

    Exo6

---
+++ exo6.py
@@ -0,0 +1,18 @@
+#Exo 6 Partie 1
+an=2023
+an_naissance=int(input("Entrez votre anné de naissance: "))
+sortie=an-an_naissance
+print("Cela était il y a ", sortie, "ans")
+
+#Exo 6 Partie 2
+exp=float(input("Entrez Exponent: "))
+num=2
+sortie_=num**exp
+print(num, "^", exp, "=", sortie_)
+
+#Exo 6 Partie 3
+cote=float(input("Entrez la longeur d'un coté du carré: "))
+aire=cote**cote
+peri=cote*4
+print("Aire: ",aire)
+print("Perimetre:", peri)
