6394d6c7107a69fcca86b98464263f345c592554
Hugo <84995264+HereLiesHugo@users.noreply.github.com>
2023-10-20 11:34:31 +0200

    Add files via upload

---
+++ exo9.py
@@ -0,0 +1,12 @@
+from math import sqrt
+
+#pour console
+n=sqrt(3)
+print(n)
+
+#pour pythagore
+c1=float(input("Entrez le premier côté: "))
+c2=float(input("Entrez le deuxieme côté: "))
+sortie=c1**2 + c2**2
+sortie=sqrt(sortie)
+print("Hypothenuse = ", sortie)
